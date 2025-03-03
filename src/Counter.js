70aec1584c6f85c08380482831945c1259c9cd39
rohnshrma <rohn11shrma@gmail.com>
2025-03-03 12:32:11 +0530

    Refactor Counter component to use Redux hooks for state management

--- src/Counter.js
+++ src/Counter.js
@@ -1,10 +1,18 @@
-import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./slices/counterSlice";
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement, reset } from "./slices/counterSlice";
+const Counter = () => {
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
 
-const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
+  return (
+    <div style={{ textAlign: "center", marginTop: "20px" }}>
+      <h1>Counter: {count}</h1>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <button onClick={() => dispatch(reset())}>Reset</button>
+    </div>
+  );
+};
 
-export default store;
+export default Counter;
